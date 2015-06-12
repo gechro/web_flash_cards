ebff1e09bdfbce4ffd6c96d8986a4e7ff1c0f159
[sevennote] <[Joseph.Fields22@gmail.com]>
2015-06-12 18:25:05 -0400

    working on error in the route for decks/:id/play

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -1,9 +1,10 @@
+require 'pry'
 enable :sessions
 
 
 get '/decks/:id/start' do
 	session['number'] = 0
-	redirect "/decks"
+	redirect ("/decks/#{params[:id]}/play")
 end
 
 
@@ -11,8 +12,8 @@ get '/decks/:id/play' do
 
   deck = Deck.find(params[:id])
 
-  if deck.cards.size <= session['number']
-	  @current_card = deck[session['number']]
+  if deck.cards.size > session['number']
+	  @card = deck[session['number']]
 	  session['number'] += 1
 	    erb :"decks/show"
   end
