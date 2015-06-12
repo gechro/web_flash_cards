f3a8c377c79acf947126b2066824530ecea0ac31
[sevennote] <[Joseph.Fields22@gmail.com]>
2015-06-12 17:41:14 -0400

    added descks/:id/start

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -1,5 +1,20 @@
+enable :sessions
+
+
+get '/decks/:id/start' do
+	session['number'] = 0
+	redirect "/decks"
+end
+
+
 get '/decks/:id/play' do
+
   deck = Deck.find(params[:id])
-  @card = deck.card.first
-    erb :"decks/show"
+
+  if deck.cards.size <= session['number']
+	  @current_card = deck[session['number']]
+	  session['number'] += 1
+	    erb :"decks/show"
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,18 +15,17 @@ users.each {|user| User.create(user)}
 #decks
 
 pokemon = [
-{:name 'pokemon', creator_id: 1}
+{name: 'pokemon', creator_id: 1}
 ]
 
-pokemon_deck = Deck.create({:name 'pokemon', creator_id: 1, category_id: 1})
+pokemon_deck = Deck.create({name: 'pokemon', creator_id: 1, category_id: 1})
 
 
 
 
 #cards
 
-pokemon_cards = [{question: 'name the pokemon that looks like an electric mouse', answer: 'pikachu',deck_id: 1}
-	,{question: 'name the first pokemon',answer: 'bulbasaur',deck_id: 1}]
+pokemon_cards = [{question: 'name the pokemon that looks like an electric mouse', answer: 'pikachu',deck_id: 1},{question: 'name the first pokemon',answer: 'bulbasaur',deck_id: 1}]
 
 
 pokemon_cards.each {|card| Card.create(card)}
